89130d83078a892a9aa5ccc35fc9413f44c02c40
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-05-27 13:56:47 +0530

    add page not found

--- myportfolio/src/page/PageNotFound.js
+++ myportfolio/src/page/PageNotFound.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "../styles/PageNotFound.module.css";
 
 function PageNotFound() {
+  const [count, setCount] = useState(5);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setCount((prevCount) => prevCount - 1);
+    }, 1000);
+
+    if (count === 0) {
+      navigate("/homepage");
+    }
+
+    return () => clearInterval(countdown);
+  }, [count, navigate]);
+
   return (
-    <div>
-      <h1>Page not found</h1>
+    <div className={styles.container}>
+      <img
+        className={styles.not_found}
+        src="/image/pagenotFound.svg"
+        alt="Page Not Found"
+      />
+      <h1 className={styles.not_found_heading}>Page Not Found</h1>
+      <p className={styles.not_found_desc}>
+        Redirecting to the homepage in {count} seconds...
+      </p>
     </div>
   );
 }
